ab174290e748919bc1dc0ad9dda75d29c49340e4
gomarcd <git@gomarcd.dev>
2023-11-25 21:30:44 +0800

    update /etc/postfix/main.cf with tls certificates

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,10 +52,8 @@ else
     echo "Database already exists. Continuing..."
 fi
 
-# CONFIGURE DOVECOT
-echo "Configuring Dovecot for SQLite..."
-
 # Add /etc/dovecot/dovecot-sql.conf configuration
+echo "Configuring Dovecot for use with database..."
 echo "Writing to /etc/dovecot/dovecot-sql.conf..."
 touch /etc/dovecot/dovecot-sql.conf
 echo "driver = sqlite
@@ -65,11 +63,10 @@ password_query = SELECT password, crypt AS userdb_mail_crypt_save_version, \
 password AS userdb_mail_crypt_private_password, username, domain \
 FROM mailbox WHERE username = '%n';" > /etc/dovecot/dovecot-sql.conf
 
-# CONFIGURE POSTFIX
-
-echo "Configuring Postfix for SQLite..."
+echo "Dovecot configured."
 
 # Create or overwrite /etc/postfix/sqlite_virtual_domains_maps.cf
+echo "Configuring Postfix for use with database..."
 echo "Writing to /etc/postfix/sqlite_virtual_domains_maps.cf..."
 echo "dbpath = $DATABASE_PATH
 query = SELECT 1 FROM virtual_domains WHERE domain='%s'" > /etc/postfix/sqlite_virtual_domains_maps.cf
@@ -84,15 +81,15 @@ echo "Writing to sqlite_virtual_alias_maps.cf..."
 echo "dbpath = $DATABASE_PATH
 query = SELECT destination FROM virtual_aliases WHERE source='%s'" > /etc/postfix/sqlite_virtual_alias_maps.cf
 
-# Check if domain has a DNS record to avoid certbot failure
-echo "Checking for DNS records..."
+echo "Postfix configured."
+
+# Function to check if a domain has a DNS record to avoid certbot failure
 check_dns() {
   local result=$(dig "$1" | awk '/^;; ANSWER SECTION:/{p=1; next} p{print; exit}')
   if [ -n "$result" ]; then
-  	echo "OK, found a valid DNS record for $1."
     return 0  # Success, domain has a DNS record
   else
-    echo "NO DNS RECORD FOUND for $1. Skipping..."
+    echo "Domain $1 does not have a DNS record. Skipping..."
     return 1  # Failure, domain does not have a DNS record
   fi
 }
@@ -128,7 +125,7 @@ fi
 echo "Registering Let's Encrypt account under admin@$DOMAIN..."
 certbot certonly --nginx --staging --non-interactive --agree-tos --email admin@$DOMAIN -d "$DOMAINS"
 
-# Certbot started nginx, stop it and let Supervisor manage nginx process
+# Certbot stared nginx, stop it and let Supervisor manage nginx process
 service nginx stop
 
 # Reference the certificate and private key paths
@@ -136,7 +133,8 @@ CERT_PATH="/etc/letsencrypt/live/$HOSTNAME/fullchain.pem"
 KEY_PATH="/etc/letsencrypt/live/$HOSTNAME/privkey.pem"
 
 # Update Dovecot configuration files with the cert and key paths
-echo "Updating Dovecot configuration with TLS certs..."
+
+echo "Updating Dovecot configuration with TLS certificates..."
 echo "Making backup of /etc/dovecot/conf.d/10-ssl.conf to /etc/dovecot/conf.d/10-ssl.bak"
 cp /etc/dovecot/conf.d/10-ssl.conf /etc/dovecot/conf.d/10-ssl.bak
 
@@ -154,6 +152,12 @@ grep -q '^\s*#*\s*ssl_key =' /etc/dovecot/conf.d/10-ssl.conf && sed -i "/^\s*#*\
 
 echo "Setting ssl_min_protocol = TLSv1.2..."
 grep -q '^\s*#*\s*ssl_min_protocol =' /etc/dovecot/conf.d/10-ssl.conf && sed -i "/^\s*#*\s*ssl_min_protocol =/s~.*~ssl_min_protocol = TLSv1.2~" /etc/dovecot/conf.d/10-ssl.conf || echo "ssl_min_protocol = TLSv1.2" >> /etc/dovecot/conf.d/10-ssl.conf
+
+# Update Postfix configuration files with the cert and key paths
+
+echo "Updating Postfix configuration with TLS certificates..."
+grep -q '^\s*#*\s*smtpd_tls_cert_file =' /etc/postfix/main.cf && sed -i "/^\s*#*\s*smtpd_tls_cert_file =/s~.*~smtpd_tls_cert_file = $CERT_PATH~" /etc/postfix/main.cf || echo "smtpd_tls_cert_file = $CERT_PATH" >> /etc/postfix/main.cf
+grep -q '^\s*#*\s*smtpd_tls_key_file =' /etc/postfix/main.cf && sed -i "/^\s*#*\s*smtpd_tls_key_file =/s~.*~smtpd_tls_key_file = $KEY_PATH~" /etc/postfix/main.cf || echo "smtpd_tls_key_file = $KEY_PATH" >> /etc/postfix/main.cf
 
 echo "Updating ssl configuration..."
 echo "Making backup of /etc/ssl/openssl.cnf..."
