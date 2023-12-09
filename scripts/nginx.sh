e11fa79d4a17131310e1eee77a2231b5e39513ee
gomarcd <git@gomarcd.dev>
2023-12-09 23:09:50 +0800

    fix cert path for mta-sts block

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -45,8 +45,8 @@ server {
 server {
     listen 443 ssl;
     server_name mta-sts.$DOMAIN;
-    ssl_certificate /etc/letsencrypt/live/mta-sts.$DOMAIN/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/mta-sts.$DOMAIN/privkey.pem;
+    ssl_certificate /etc/letsencrypt/live/$HOSTNAME/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$HOSTNAME/privkey.pem;
     ssl_protocols TLSv1.2 TLSv1.3;
     ssl_ciphers 'TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384';
 
